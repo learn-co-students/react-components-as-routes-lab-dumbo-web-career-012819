702601455f35263eefccf9b552e7b585d8338c54
github username <github email address>
2019-04-12 13:33:22 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+    const display =
+         directors.map(director => {
+            return (<div>
+               <h1>{director.name}</h1>
+               <ul>
+                 { director.movies.map(movie=>{
+                   return <li>{movie}</li>
+             })}
+               </ul>
+               </div>
+           )
+        })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors</h1>
+      {display}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+    const display =
+         movies.map(movie => {
+            return (<div>
+               <h1>Name: {movie.title}</h1>
+               <p>Time: {movie.time}</p>
+               <p>Genres:</p>
+               <ul>
+                 { movie.genres.map(genre=>{
+                   return <li>{genre}</li>
+             })}
+               </ul>
+               </div>
+           )
+        })
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies</h1>
+        {display}
     </div>
   );
 };
